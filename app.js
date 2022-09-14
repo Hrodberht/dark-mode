e9fb4b78fa4d1c62aae307f612350ed56f914241
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-14 08:41:08 +0100

    Set up data.js import

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import articles from "./data.js";
+
 const toggleBtn = document.querySelector(".btn");
 
 toggleBtn.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-theme");
 });
+
+console.log(articles);
